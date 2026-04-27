a5b55820f1d4b1fd2714bbfd759e0577c829a32b
Jesus Camposeco <camosecocardenas@gmail,com>
2026-04-26 21:17:45 -0600

    test 22

--- demo/src/main/java/com/example/app/demo/controllers/HelloController.java
+++ demo/src/main/java/com/example/app/demo/controllers/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-// Change 21
+// Change 22
     @GetMapping("/hello")
     public String hello() {
         return helloService.getHelloMessage();
